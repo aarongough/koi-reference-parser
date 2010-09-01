bac6e468fdc712fe2a12362f21f6ab88d6c11ae0
Aaron Gough <aaron@aarongough.com>
2010-09-01 12:41:12 -0400

    Restructed tests for assignment to/from hashes.

--- test/unit/assignment/assignment_to_hash_test.rb
+++ test/unit/assignment/assignment_to_hash_test.rb
@@ -1,9 +1,9 @@
-require File.expand_path(File.join(File.dirname(__FILE__), '..', 'test_helper.rb'))
+require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'test_helper.rb'))
 
-class HashAccessorTest < Test::Unit::TestCase
+class AssignmentToHashTest < Test::Unit::TestCase
 
   include KoiReferenceParser
-
+  
   test "assignment to hash key" do
     tree = Parser.parse('test[1] = 1')
     assert_kind_of Block, tree
@@ -24,18 +24,6 @@ class HashAccessorTest < Test::Unit::TestCase
     assert_kind_of HashAccessorList, tree.elements.first.elements.first.elements[1]
     assert_kind_of HashAccessor, tree.elements.first.elements.first.elements[1].elements.first
     assert_equal 3, tree.elements.first.elements.first.elements[1].elements.length
-  end
-  
-  test "assignment of hash key" do
-    tree = Parser.parse('test = test[1]')
-    assert_kind_of Block, tree
-    assert_kind_of Statement, tree.elements.first
-    assert_kind_of Assignment, tree.elements.first.elements.first
-    assert_kind_of Identifier, tree.elements.first.elements.first.elements[0]
-    assert_kind_of Expression, tree.elements.first.elements.first.elements[2]
-    assert_kind_of HashAccess, tree.elements.first.elements.first.elements[2].elements.first
-    assert_kind_of Identifier, tree.elements.first.elements.first.elements[2].elements.first.elements[0]
-    assert_kind_of HashAccessorList, tree.elements.first.elements.first.elements[2].elements.first.elements[1]
   end
 
 end
