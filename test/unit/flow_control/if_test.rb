bb6b7e4f123d6559a4d7b1cf5a1f018006b25540
Aaron Gough <aaron@aarongough.com>
2010-09-01 12:45:13 -0400

    Split tests with common themes into their own folders.

--- test/unit/flow_control/if_test.rb
+++ test/unit/flow_control/if_test.rb
@@ -1,23 +1,23 @@
-require File.expand_path(File.join(File.dirname(__FILE__), '..', 'test_helper.rb'))
+require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'test_helper.rb'))
 
-class UnlessTest < Test::Unit::TestCase
+class IfTest < Test::Unit::TestCase
 
   include KoiReferenceParser
 
-  test "should parse unless statement" do
-    tree = Parser.parse("unless(1 == 1)\n test = 1\nend")
+  test "should parse if statement" do
+    tree = Parser.parse("if(1 == 1)\n test = 1\nend")
     assert_kind_of Block, tree
     assert_kind_of Statement, tree.elements.first
-    assert_kind_of Unless, tree.elements.first.elements.first
+    assert_kind_of If, tree.elements.first.elements.first
     assert_kind_of Expression, tree.elements.first.elements.first.elements.first
     assert_kind_of Block, tree.elements.first.elements.first.elements.last
   end
   
-  test "should parse single line unless statement" do
-    tree = Parser.parse("unless(1 == 1) test = 1; end")
+  test "should parse single line if statement" do
+    tree = Parser.parse("if(1 == 1) test = 1; end")
     assert_kind_of Block, tree
     assert_kind_of Statement, tree.elements.first
-    assert_kind_of Unless, tree.elements.first.elements.first
+    assert_kind_of If, tree.elements.first.elements.first
     assert_kind_of Expression, tree.elements.first.elements.first.elements.first
     assert_kind_of Block, tree.elements.first.elements.first.elements.last
   end
